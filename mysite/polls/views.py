fd1739aa4f2f2dfc0c6a28505705782bbf582e87
Pete Russell <pinkbatz@gmail.com>
2017-09-01 12:53:19 -0500

    Adds templates, using manual template loader.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Polls index.")
+    latest_questions_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_questions_list': latest_questions_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
